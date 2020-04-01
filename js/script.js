6bb002a0f779938e50dff067480ea8f0c3a77a41
Andrew <Andrew.Cao@stonybrook.edu>
2020-04-01 00:14:56 -0400

    Changed the script file for all pages to index.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ var slider1 = document.getElementById("admissionRate");
 var output1 = document.getElementById("admissionResult");
 output1.innerHTML = slider1.value;
 
-slider1.oninput = function () {
+slider1.oninput = function update1() {
     output1.innerHTML = this.value * 100 + "%";
 }
 
@@ -21,7 +21,7 @@ var slider2 = document.getElementById("rank");
 var output2 = document.getElementById("rankResult");
 output2.innerHTML = slider2.value;
 
-slider2.oninput = function () {
+slider2.oninput = function update2() {
     output2.innerHTML = this.value;
 }
 
@@ -29,6 +29,6 @@ var slider3 = document.getElementById("size");
 var output3 = document.getElementById("sizeResult");
 output3.innerHTML = slider3.value;
 
-slider3.oninput = function () {
+slider3.oninput = function update3() {
     output3.innerHTML = this.value;
 }
