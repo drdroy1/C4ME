0933fd16d98035f7074a62e279f35fa552beb4d3
root <root@maq416.c.c4me-268617.internal>
2020-04-01 01:06:16 +0000

    Added search functionality

--- student.js
+++ student.js
@@ -11,29 +11,38 @@ const south = ['OK', 'AR', 'KY', 'WV', 'MD', 'DE', 'TX', 'LA', 'TN', 'DC', 'MS',
 const pacific = ['HI', 'AK'];
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/search', function(req, res){
-	res.sendFile(__dirname + '/html/search_college.html');
+app.get('', function(req, res){
+	res.sendFile(__dirname + '/html/search_colleges.html');
 });
 
-app.post('/search', function(req, res) {
+app.post('', function(req, res) {
+	console.log(req.body)
 	let cname = req.body.collegeName;
-	let adminRate = req.body.admimissionRate;
-	let cost = req.body.cost;
+	let adminRate = parseInt(req.body.admimissionRate, 10);
+	let cost = parseInt(req.body.cost, 10);
 	let loc = req.body.loc;
-	let size = req.body.size;
+	let size = parseInt(req.body.size, 10);
 	let m1 = req.body.m1;
 	let m2 = req.body.m2;
 	
-	let query = {cname: cname, size: { $gte: size }, admission:{percent:{$gte:adminRate}}}
-
+	//let query = {name: cname, size: { $gte: size }, admission:{percent:{$gte:adminRate}}}	
+	let query = {name:cname, size : {$gte: size}}
+	console.log('query: ' + JSON.stringify(query))
+	
 	mongoClient.connect(mongodb, function (err, db) {
 		if (err) throw err;
 		let currentDB = db.db('c4me');
-		currentDB.collection('college').find(query).toArray(function(err, result){
+		currentDB.collection('college').find(query).limit(30).toArray(function(err, result){
 			if (err) throw err;
-			console.log(result)
+			if(result != null){
+				console.log(result[0].majors)
+				console.log(result[0].majors[0])
+			}
 		});
+		res.end()
 	});
 });
 
